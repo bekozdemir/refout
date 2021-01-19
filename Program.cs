a6aeb8100890cc64f075010b03e22ffceae50145
Berkay <brkyzdmr@gmail.com>
2021-01-19 13:39:37 +0300

    3. gün ödev 2 kodu

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace refout
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 200;
+            int n2 = 10;
+
+            Subtract subt = new Subtract();
+            subt.subtract(ref n1,n2);
+
+            Subtract subt2 = new Subtract();
+            subt2.subtract2(out int n3, n2);
+
+        }
+    }
+}
---
+++ Subtract.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace refout
+{
+    class Subtract
+    {
+        public void subtract(ref int n1, int n2) 
+        {
+            n1 = 40;
+            int result = n1 - n2;
+            Console.WriteLine("result : " + result);
+        }
+
+
+        public void subtract2(out int n3, int n2)
+        {
+            n3 = 100;
+            int result2 = n3 - n2;
+            Console.WriteLine("result2 : " + result2);
+        }
+    }
+}
